5f7de47be94edd85ba0027c5e889c3bfe3e8b3ac
senadakadric <skadric@fordham.edu>
2020-06-10 19:06:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with TitleValidator
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 end
---
+++ app/validators/title_validator.rb
@@ -0,0 +1,7 @@
+class TitleValidator < ActiveModel::Validator
+    def validate(record)
+        valid_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        
+        
+    end
+end
